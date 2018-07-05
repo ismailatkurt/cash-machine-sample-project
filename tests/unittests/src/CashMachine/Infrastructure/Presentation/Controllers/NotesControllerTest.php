ee9b506dda3890114361333c71a805c33b27c84e
İsmail Atkurt <ismail.atkurt@gmail.com>
2018-07-05 11:24:22 +0200

    adding UT. Update build status link.

--- tests/unittests/src/CashMachine/Infrastructure/Presentation/Controllers/NotesControllerTest.php
+++ tests/unittests/src/CashMachine/Infrastructure/Presentation/Controllers/NotesControllerTest.php
@@ -2,9 +2,37 @@
 
 namespace CashMachine\Tests\Note\Infrastructure\Presentation\Controllers;
 
+use CashMachine\Note\Application\Services\NoteService;
+use CashMachine\Note\Infrastructure\Presentation\Controllers\NotesController;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\InputFilter\InputFilter;
 
 class NotesControllerTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function construct_test()
+    {
+        // prepare
+        /** @var ServerRequestInterface|\PHPUnit_Framework_MockObject_MockObject $requestMock */
+        $requestMock = $this->createMock(ServerRequestInterface::class);
 
+        /** @var ResponseInterface|\PHPUnit_Framework_MockObject_MockObject $responseMock */
+        $responseMock = $this->createMock(ResponseInterface::class);
+
+        /** @var InputFilter|\PHPUnit_Framework_MockObject_MockObject $inputFilterMock */
+        $inputFilterMock = $this->createMock(InputFilter::class);
+
+        /** @var NoteService|\PHPUnit_Framework_MockObject_MockObject $noteServiceMock */
+        $noteServiceMock = $this->createMock(NoteService::class);
+
+        // test
+        $classUnderTest = new NotesController($requestMock, $responseMock, $inputFilterMock, $noteServiceMock);
+
+        // verify
+        $this->assertInstanceOf(NotesController::class, $classUnderTest);
+    }
 }
